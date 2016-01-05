c5e467e9ab51097702aea62905b947b12f9f5f88
Xiao cong <1498439067@qq.com>
2016-01-04 19:07:35 -0800

    MultiThread Test in NewTx

--- impl/src/test/java/org/opendaylight/distributed/tx/impl/DistributedTxProviderTest.java
+++ impl/src/test/java/org/opendaylight/distributed/tx/impl/DistributedTxProviderTest.java
@@ -7,8 +7,11 @@
  */
 package org.opendaylight.distributed.tx.impl;
 
+import com.google.common.util.concurrent.CheckedFuture;
+import com.sun.jmx.snmp.tasks.ThreadService;
 import org.eclipse.xtend.lib.annotations.Data;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.internal.util.collections.Sets;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
@@ -22,11 +25,24 @@ import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.fail;
 
 public class DistributedTxProviderTest {
     private TxProvider txProvider;
+    private DTxProviderImpl dTxProvider;
+    private DTx dTx1;
+    private DTx dTx2;
+    private DTx dTx3;
+
+    InstanceIdentifier<TestClassNode1> node1 = InstanceIdentifier.create(TestClassNode1.class);
+    InstanceIdentifier<TestClassNode2> node2 = InstanceIdentifier.create(TestClassNode2.class);
+    InstanceIdentifier<TestClassNode3> node3 = InstanceIdentifier.create(TestClassNode3.class);
+    InstanceIdentifier<TestClassNode4> node4 = InstanceIdentifier.create(TestClassNode4.class);
+    InstanceIdentifier<TestClassNode5> node5 = InstanceIdentifier.create(TestClassNode5.class);
 
     private class myTxProvider implements TxProvider{
 
@@ -75,10 +91,83 @@ public class DistributedTxProviderTest {
         }
     }
 
+    private class CreateNewTx1 implements Runnable{
+
+        @Override
+        public void run() {
+
+            int sleepTime = (int)(Math.random()*100);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Set<InstanceIdentifier<?>> nodeSet = Sets.newSet(node1, node2, node3);
+            try
+            {
+               dTx1 = dTxProvider.newTx(nodeSet);
+            }catch (Exception e)
+            {
+                Assert.assertTrue(e instanceof DTxException.DTxInitializationFailedException);
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private class CreateNewTx2 implements Runnable{
+
+        @Override
+        public void run() {
+
+            int sleepTime = (int)(Math.random()*100);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Set<InstanceIdentifier<?>> nodeSet = Sets.newSet(node3, node4, node5);
+            try
+            {
+               dTx2 = dTxProvider.newTx(nodeSet);
+            }catch (Exception e)
+            {
+                Assert.assertTrue(e instanceof DTxException.DTxInitializationFailedException);
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private class CreateNewTx3 implements Runnable{
+
+        @Override
+        public void run() {
+
+            int sleepTime = (int)(Math.random()*100);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Set<InstanceIdentifier<?>> nodeSet = Sets.newSet(node4, node5);
+            try
+            {
+                dTx3 = dTxProvider.newTx(nodeSet);
+            }catch (Exception e)
+            {
+                Assert.assertTrue(e instanceof DTxException.DTxInitializationFailedException);
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 
     public void addProvider(final TxProvider txProvider) {
         this.txProvider = txProvider;
     }
+
     @Test
     public void testOnSessionInitiated() {
         DTxProviderImpl provider = new DTxProviderImpl(txProvider);
@@ -87,32 +176,70 @@ public class DistributedTxProviderTest {
     @Test
     public void testNewTx()
     {
-        InstanceIdentifier<TestClassNode1> node1 = InstanceIdentifier.create(TestClassNode1.class);
-        InstanceIdentifier<TestClassNode2> node2 = InstanceIdentifier.create(TestClassNode2.class);
-        InstanceIdentifier<TestClassNode3> node3 = InstanceIdentifier.create(TestClassNode3.class);
-        InstanceIdentifier<TestClassNode4> node4 = InstanceIdentifier.create(TestClassNode4.class);
-        InstanceIdentifier<TestClassNode5> node5 = InstanceIdentifier.create(TestClassNode5.class);
 
-        DTxProviderImpl dTxProvider = new DTxProviderImpl(new myTxProvider());
-
-        Set<InstanceIdentifier<?>> nodeSet1 = Sets.newSet(node1, node2, node3);
-        Set<InstanceIdentifier<?>> nodeSet2 = Sets.newSet(node3, node4, node5);
-        Set<InstanceIdentifier<?>> nodeSet3 = Sets.newSet(node4, node5);
-
-        DTx dTx1 = dTxProvider.newTx(nodeSet1);
-
-        try
-        {
-            DTx dTx2 = dTxProvider.newTx(nodeSet2);
-            fail();
-        }catch (Exception e)
-        {
-            Assert.assertTrue(e instanceof DTxException.DTxInitializationFailedException);
-            System.out.println(e.getMessage());
+        dTxProvider = new DTxProviderImpl(new myTxProvider());
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        threadPool.execute(new CreateNewTx1());
+        threadPool.execute(new CreateNewTx2());
+        threadPool.execute(new CreateNewTx3());
+        threadPool.shutdown();
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        DTx dTx3 = dTxProvider. newTx(nodeSet3);
+    }
 
+    @Test
+    public void testCancel()
+    {
+        Runnable createDtx1 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    DTx dTx = dTxProvider.newTx((Set<InstanceIdentifier<?>>)Sets.newSet(node1, node2, node3));
+                }catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                    return;
+                }
+                dTx1.cancel();
 
+            }
+        };
+
+        Runnable createDtx2 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    DTx dTx = dTxProvider.newTx((Set<InstanceIdentifier<?>>)Sets.newSet(node2, node3, node4));
+                }catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                    fail();
+                }
+            }
+        };
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        threadPool.execute(createDtx1);
+        threadPool.execute(createDtx2);
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
