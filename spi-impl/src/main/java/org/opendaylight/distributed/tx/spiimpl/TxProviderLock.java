e7eb3003cfc6ad5b1318db81bff8f5df009e6300
Fangming Chai <fachai@cisco.com>
2016-05-13 16:12:14 -0700

    add comments to spi-impl

--- spi-impl/src/main/java/org/opendaylight/distributed/tx/spiimpl/TxProviderLock.java
+++ spi-impl/src/main/java/org/opendaylight/distributed/tx/spiimpl/TxProviderLock.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.distributed.tx.spiimpl;
 
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -12,22 +19,16 @@ public class TxProviderLock {
     final private Set<InstanceIdentifier<?>> lockSet = new HashSet<>();
 
     public boolean isDeviceLocked(InstanceIdentifier<?> device) {
-
-        return false;
-        /*
         boolean ret ;
         synchronized (TxProviderLock.this) {
             ret = lockSet.contains(device);
         }
 
         return ret;
-        */
     }
 
     public boolean lockDevices(Set<InstanceIdentifier<?>> deviceSet) {
         boolean ret = true;
-
-        if(false)
 
         synchronized (TxProviderLock.this) {
             Set<InstanceIdentifier<?>> s = new HashSet<>();
@@ -46,7 +47,6 @@ public class TxProviderLock {
     }
 
     public void releaseDevices(Set<InstanceIdentifier<?>> deviceSet) {
-        if(false)
         synchronized (TxProviderLock.this) {
             this.lockSet.removeAll(deviceSet);
         }
